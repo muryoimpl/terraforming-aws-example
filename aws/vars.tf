ee6a02c370bab631611ce14bdbae86bc225df91b
muryoimpl <muryoimpl@gmail.com>
2018-02-13 02:05:45 +0900

    Add vpc

--- aws/vars.tf
+++ aws/vars.tf
@@ -1,7 +1,17 @@
+# variables which are changed by environment
+variable "env_name" {
+  default = "stg"
+}
+
+# variables which are not changed by environment
 variable "region" {
   default = "ap-northeast-1"
 }
 
 variable "app_name" {
   default = "talker-signupper"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }

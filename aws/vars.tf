82ff1470798ec155cceb20631a2c2a83d78ca4c7
muryoimpl <muryoimpl@gmail.com>
2018-02-13 02:07:04 +0900

    Add subnet

--- aws/vars.tf
+++ aws/vars.tf
@@ -15,3 +15,27 @@ variable "app_name" {
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr_a" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr_c" {
+  default = "10.0.3.0/24"
+}
+
+variable "availability_zone_a" {
+  default = "ap-northeast-1a"
+}
+
+variable "availability_zone_c" {
+  default = "ap-northeast-1c"
+}
+
+variable "ami_id" {
+  default = "ami-e3166185"
+}

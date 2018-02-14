90428ad4e56027803f2bb645f2ac6db1d37070a4
muryoimpl <muryoimpl@gmail.com>
2018-02-15 03:09:39 +0900

    Revise egress of security group

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type          = "t2.micro"
   subnet_id              = "${aws_subnet.public_subnet.id}"
   key_name               = "${aws_key_pair.auth.id}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids = ["${aws_security_group.ts_bastion_sg.id}"]
 
   tags {
     Name = "bastion"
@@ -15,7 +15,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   subnet_id              = "${aws_subnet.private_subnet_a.id}"
   key_name               = "${aws_key_pair.auth.id}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids = ["${aws_security_group.ts_web_sg.id}"]
 
   tags {
     Name = "web"
